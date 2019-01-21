f29fd8c4671c1d6d7145f06f4cdde0786b44f234
Daniel Elgh <daniel.elgh@gmail.com>
2019-01-21 16:24:37 +0100

    Replaced std::random_shuffle() with a basic shuffle function

--- main.cpp
+++ main.cpp
@@ -1,25 +1,32 @@
 #include <cstdio>
-#include <algorithm>
+#include <cstdlib>
 #include <ctime>
 
-int randomizer(int i) { return std::rand() % i; }
+void shuffle_deck(unsigned int arr[], size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        unsigned int tmp = arr[i];
+        unsigned int rand = std::rand() % size;
+        arr[i] = arr[rand];
+        arr[rand] = tmp;
+    }
+}
 
 int main() {
     const unsigned int rounds = 10000000U;
     unsigned int lost = 0;
-    unsigned int deck[] = { 
+    unsigned int deck[] = {
         1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0
     };
-    const unsigned int deckSize = sizeof(deck) / sizeof(*deck);
-    std::srand(unsigned(std::time(0)));
+    const size_t deckSize = sizeof(deck) / sizeof(*deck);
+    std::srand(unsigned(std::time(NULL)));
     for (unsigned int played = 0; played < rounds; played++) {
-        std::random_shuffle(&deck[0], &deck[deckSize], randomizer);
+        shuffle_deck(deck, deckSize);
         unsigned int n = 0;
-        for (unsigned int i = 0; i <= deckSize; i++) {
+        for (size_t i = 0; i < deckSize; i++) {
             (n == 3) ? n = 1 : n++;
             if (deck[i] == n) {
                 lost++;
