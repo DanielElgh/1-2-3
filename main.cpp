5ce2971a6dbb6a7585dd55eacf7ec5e29543847e
Daniel Elgh <daniel.elgh@gmail.com>
2019-08-25 10:36:50 +0200

    Revised deck array initialization

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <cstdlib>
 #include <ctime>
 
-void shuffle_deck(unsigned int arr[], size_t size) {
+void shuffle_deck(unsigned int arr[], const size_t size) {
     for (size_t i = 0; i < size; i++) {
         unsigned int tmp = arr[i];
         unsigned int rand = std::rand() % size;
@@ -13,15 +13,11 @@ void shuffle_deck(unsigned int arr[], size_t size) {
 
 int main() {
     const unsigned int rounds = 10000000U;
+    const size_t deckSize = 52;
     unsigned int lost = 0;
-    unsigned int deck[] = {
-        1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0
+    unsigned int deck[deckSize] = {
+        1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3
     };
-    const size_t deckSize = sizeof(deck) / sizeof(*deck);
     std::srand(unsigned(std::time(NULL)));
     for (unsigned int played = 0; played < rounds; played++) {
         shuffle_deck(deck, deckSize);
